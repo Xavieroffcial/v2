88b6552c4f88f244c039e4a1491ef9622e3e7726
Xavieroffcial <108806813+Xavieroffcial@users.noreply.github.com>
2022-08-17 13:45:10 +0800

    Update tag-owner.js

--- plugins/tag-owner.js
+++ plugins/tag-owner.js
@@ -5,7 +5,7 @@ let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 let jarot = 'https://telegra.ph/file/966adedcd3b40f50fa794.jpg'
 await conn.send2ButtonImg(m.chat, jarot, "*BIMLEK🐦*", 'ngapain banh tag” dia', 'OWNER', '.owner', 'KEMBALI', '.menu', m)                        
 }
-handler.customPrefix = /@6285850539404,79609055704/i
+handler.customPrefix = /@6283862006229,6283862006229/i
 handler.command = new RegExp
 
 module.exports = handler
